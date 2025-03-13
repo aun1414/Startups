9e373f9908738c094cbbcdb69acfb1ce95f28150
aun1414 <abbasaun1414@gmail.com>
2025-03-13 16:11:58 -0400

    Sanity Write Client

---
+++ sanity/lib/write-client.ts
@@ -0,0 +1,18 @@
+import "server-only"
+
+import { createClient } from 'next-sanity'
+
+import { apiVersion, dataset, projectId,token } from '../env'
+import { write } from 'fs';
+
+export const writeClient = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  token,
+});
+
+if(!writeClient.config().token){
+  throw new Error('Token is missing')
+}
