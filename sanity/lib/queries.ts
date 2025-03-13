064d5de8acf08de9063ff77d829ac5c75fa843d7
aun1414 <abbasaun1414@gmail.com>
2025-03-13 15:18:50 -0400

    Startup details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -31,3 +31,34 @@ export const STARTUP_BY_ID_QUERY =
   image,
   pitch,
 }`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0]{
+      _id, views
+  }
+`);
+
+export const PLAYLIST_BY_SLUG_QUERY =
+  defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+      _id,
+      name,
+      slug,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }
+}`);
